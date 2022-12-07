e2e7ffd5275be69f0bb79abe90fe7612d093048f
kontikiki <orphesia27@gmail.com>
2022-12-07 15:25:56 +0900

    add PWM_software project

---
+++ usart_serial_test/STM32CubeIDE/Debug/Application/User/Startup/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Application/User/Startup/startup_stm32f429zitx.s 
+
+OBJS += \
+./Application/User/Startup/startup_stm32f429zitx.o 
+
+S_DEPS += \
+./Application/User/Startup/startup_stm32f429zitx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Application/User/Startup/%.o: ../Application/User/Startup/%.s Application/User/Startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
+clean: clean-Application-2f-User-2f-Startup
+
+clean-Application-2f-User-2f-Startup:
+	-$(RM) ./Application/User/Startup/startup_stm32f429zitx.d ./Application/User/Startup/startup_stm32f429zitx.o
+
+.PHONY: clean-Application-2f-User-2f-Startup
+
